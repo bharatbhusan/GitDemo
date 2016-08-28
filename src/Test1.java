ec06b1b1be069d0310bb39baacdc1c4fbcf798ec
bharatbhusanmantry <bharatbhusanmantry@gmail.com>
2016-08-28 23:04:48 +0530

    Second Commit

--- src/Test1.java
+++ src/Test1.java
@@ -4,7 +4,8 @@ public class Test1 {
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		System.out.println("My First Git Project");
+		System.out.println("My First Git Testing Project");
+		System.out.println("Thank you Mangu");
 	}
 
 }
